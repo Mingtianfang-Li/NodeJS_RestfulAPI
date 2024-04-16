0a9b5fa47291d09323f2d36f134692baf9dea4c7
Ree Li <li.mingt@northeastern.edu>
2024-04-16 15:04:37 -0700

    adding login function

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -2,6 +2,31 @@ import { createUser, getUserByEmail } from '../db/user';
 import express from 'express';
 import { authentication, random } from '../helpers';
 
+export const login = async (req: express.Request, res: express.Response) => {
+    try {
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return res.sendStatus(400);
+        }
+
+        const user = await getUserByEmail(email);
+
+        if (!user) {
+            return res.sendStatus(400);
+        }
+
+        const expectedHash = authentication(user.authentication.salt, password);
+
+        if (user.authentication.password != expectedHash) {
+            return res.sendStatus(403);
+        }
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(400);
+    }
+}
+
 export const register = async (req: express.Request, res: express.Response) => {
     try {
         const { email, password, username} = req.body;
