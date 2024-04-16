5c2c7d510148a4b1ca821cfdcdd18890ce4125dc
Ree Li <li.mingt@northeastern.edu>
2024-04-16 12:55:07 -0700

    adding authentication controller and user db

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
+import mongoose from 'mongoose';
 
 const app = express();
 
@@ -22,4 +23,8 @@ server.listen(8080, () => {
 });
 
 const MONGO_URL = 'mongodb+srv://limingtianfang:QysNtjJilscCnjvL@cluster0.fqsmivz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (error: Error) => console.log(error));
 
