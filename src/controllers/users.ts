f532614bd5a5c73330cf88f308d7658cb4dcafc3
Ree Li <li.mingt@northeastern.edu>
2024-04-16 17:18:44 -0700

    adding deleteUser function

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { getUsers } from '../db/user';
+import { deleteUserById, getUsers } from '../db/user';
 
 export const getAllUsers = async (req: express.Request, res: express.Response) => {
     try {
@@ -8,6 +8,19 @@ export const getAllUsers = async (req: express.Request, res: express.Response) =
 
         return res.status(200).json(users);
     } catch(error) {
+        console.log(error);
+        return res.sendStatus(400);
+    }
+}
+
+export const deleteUser = async (req: express.Request, res: express.Response) => {
+    try {
+        const { id } = req.params;
+
+        const deleteUser = await deleteUserById(id);
+
+        return res.json(deleteUser);
+    } catch (error) {
         console.log(error);
         return res.sendStatus(400);
     }
--- src/router/users.ts
+++ src/router/users.ts
@@ -5,4 +5,5 @@ import { isAuthenticated } from '../middlewares';
 
 export default (router: express.Router) => {
     router.get('/users', isAuthenticated, getAllUsers);
-}
+};
+
